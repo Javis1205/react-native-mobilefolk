6a3140dfe210843239f8c770272f7f977ced62ef
Daind <dai@mobilefolk.com>
2019-04-09 14:25:44 +0700

    init

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const chalk       = require('chalk');
 const clear       = require('clear');
 const figlet      = require('figlet');
@@ -10,13 +11,3 @@ console.log(
     figlet.textSync('Mobilefolk Team', { horizontalLayout: 'full' })
   )
 );
-// if (files.directoryExists('.git')) {
-//     console.log(chalk.red('Already a git repository!'));
-//     process.exit();
-// }
-const run = async () => {
-    const credentials = await inquirer.askGithubCredentials();
-    console.log(credentials);
-}
-
-run();
